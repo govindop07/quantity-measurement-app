f8cb4de87679b08fb8b864234c34cfc199a292e5
govindop07 <govindupa24@gmail.com>
2026-02-18 21:53:09 +0530

    [Govind Upadhyay] Add. Added second use case of the quantity measurement app

---
+++ feature_UC2-InchEquality/src/main/java/bridgeLabz/quantity_measurement/QuantityMeasurementApp.java
@@ -0,0 +1,76 @@
+package bridgeLabz.quantity_measurement;
+
+public class QuantityMeasurementApp {
+
+	// Inner class to represent Feet measurement
+	public static class Feet {
+		private final double value;
+
+		public Feet(double value) {
+			this.value = value;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null || getClass() != obj.getClass())
+				return false;
+
+			Feet feet = (Feet) obj;
+			return Double.compare(feet.value, value) == 0;
+		}
+
+		@Override
+		public int hashCode() {
+			return Double.hashCode(value);
+		}
+	}
+
+	// Inner class to represent Inches measurement
+	public static class Inches {
+		private final double value;
+
+		public Inches(double value) {
+			this.value = value;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null || getClass() != obj.getClass())
+				return false;
+
+			Inches inches = (Inches) obj;
+			return Double.compare(inches.value, value) == 0;
+		}
+
+		@Override
+		public int hashCode() {
+			return Double.hashCode(value);
+		}
+	}
+
+	// Static method to demonstrate Feet equality
+	public static void demonstrateFeetEquality() {
+		Feet f1 = new Feet(1.0);
+		Feet f2 = new Feet(1.0);
+
+		System.out.println("Feet Equal: " + f1.equals(f2));
+	}
+
+	// Static method to demonstrate Inches equality
+	public static void demonstrateInchesEquality() {
+		Inches i1 = new Inches(1.0);
+		Inches i2 = new Inches(1.0);
+
+		System.out.println("Inches Equal: " + i1.equals(i2));
+	}
+
+	// Main method
+	public static void main(String[] args) {
+		demonstrateFeetEquality();
+		demonstrateInchesEquality();
+	}
+}
