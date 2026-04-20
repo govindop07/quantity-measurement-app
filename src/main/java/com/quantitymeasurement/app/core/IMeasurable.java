ae3e4ee3f2101f162083a26d2bf1d49ef43f4529
govindop07 <govindupa24@gmail.com>
2026-04-20 12:27:43 +0530

    Removed old monolith src folder after microservices migration

--- src/main/java/com/quantitymeasurement/app/core/IMeasurable.java
+++
@@ -1,6 +0,0 @@
-package com.quantitymeasurement.app.core;
-
-public interface IMeasurable {
-    double toBaseUnit(double value);
-    double fromBaseUnit(double value);
-}
--- src/main/java/com/quantitymeasurement/app/repository/IQuantityMeasurementRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quantitymeasurement.app.repository;
-
-import com.quantitymeasurement.app.entity.QuantityMeasurementEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface IQuantityMeasurementRepository extends JpaRepository<QuantityMeasurementEntity, Long> {
-    List<QuantityMeasurementEntity> findByUserEmail(String userEmail);
-}
